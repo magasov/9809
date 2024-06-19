476f695aec1dab38d1518f5c44c6aaf4fd197e09
magasoov <77aushev@mail.ru>
2024-06-19 13:28:35 +0300

    213

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -12,28 +12,10 @@ ScrollReveal().reveal('.home3, .two', { delay: 500, origin: 'bottom' });
 
 function toggleVisibility() {
     let svgElement = document.getElementById('navbar__header1');
-    if (svgElement.style.display === 'none') {
-        svgElement.style.display = 'block';
-    } else {
+    if (svgElement.style.display === 'block') {
         svgElement.style.display = 'none';
+    } else {
+        svgElement.style.display = 'block';
     }
 }
 
-
-//  СДЕЛАЙ ЧТОБЫ ПРИ КАЖДОМ КЛИКЕ НА ЭТОТ SVG БЛОК СКРЫВАЛСЯ И ОТКРЫВАЛСЯ <svg class="ham hamRotate ham1" viewBox="0 0 100 100" width="50" onclick="this.classList.toggle('active')">
-//             <path
-//             class="line top"
-//             d="m 30,33 h 40 c 0,0 9.044436,-0.654587 9.044436,-8.508902 0,-7.854315 -8.024349,-11.958003 -14.89975,-10.85914 -6.875401,1.098863 -13.637059,4.171617 -13.637059,16.368042 v 40" />
-//       <path
-//             class="line middle"
-//             d="m 30,50 h 40" />
-//       <path
-//             class="line bottom"
-//             d="m 30,67 h 40 c 12.796276,0 15.357889,-11.717785 15.357889,-26.851538 0,-15.133752 -4.786586,-27.274118 -16.667516,-27.274118 -11.88093,0 -18.499247,6.994427 -18.435284,17.125656 l 0.252538,40" />
-//     </svg >
-
-// ВОТ ФУНКЦИЯ
-// function hamburgerMenu() {
-//     let menu = document.querySelector("#navbar__header1")
-//     menu.style.display = "block"
-// }
